778fafa60bf0d201213a1754dd5479e75d9f6f86
Axlzs <90036459+Axlzs@users.noreply.github.com>
2025-02-10 13:13:45 +0200

    Initial commit

---
+++ api/api/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+#from django.http import HttpResponse
+
+def homepage(request):
+    #return HttpResponse("Hello world!")
+    return render(request, 'home.html')
+    
+def about(request):
+    #return HttpResponse("About page")
+    return render(request, 'about.html')
+
+    
