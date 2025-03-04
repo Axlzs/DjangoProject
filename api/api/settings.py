b9e85130e032a4350920dbdca80d2c97ccbc017b
Axlzs <90036459+Axlzs@users.noreply.github.com>
2025-03-04 12:56:16 +0200

    fix: minor misspell

--- api/api/settings.py
+++ api/api/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'mdeia/'
+MEDIA_URL = 'media/'
 
 STATIC_ROOT = BASE_DIR / 'assets'
 MEDIA_ROOT = BASE_DIR / 'media/'
